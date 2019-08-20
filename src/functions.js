83b4f19249312f430c697d556c3233fa46244b25
Amir Bizimana <amirbzmn@gmail.com>
2019-08-20 12:10:56 -0400

    set up and tear down: beforeAll(),afterAll(),beforeEach(),afterEach(), describe()

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,33 @@
 const axios = require("axios");
 
+// //Initialize a database before each function then clear it after each function
+
+// // beforeEach(() => initDatabase());
+// // afterEach(() => closedDatabase());
+
+// // Initialize a database before all functions then clear it after all functions have run
+// beforeAll(() => initDatabase());
+// afterAll(() => closedDatabase());
+
+// const initDatabase = () => console.log(`Database Initialized...`);
+// const closedDatabase = () => console.log(`Databse closed...`);
+
+const nameCheck = () => console.log("Checking Name...");
+
+describe("Checking Names", () => {
+  beforeEach(() => nameCheck());
+
+  test("user is Jeff", () => {
+    const user = "Jeff";
+    expect(user).toBe("Jeff");
+  });
+
+  test("user is Karen", () => {
+    const user = "Karen";
+    expect(user).toBe("Karen");
+  });
+});
+
 const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
